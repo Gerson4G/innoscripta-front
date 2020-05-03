9b5046041c3c595ce2ed4ea2a88cf77617c1fbff
Gerson Goatache <gersongoatache@gmail.com>
2020-05-02 23:34:31 -0400

    quantity into shopping cart

--- src/components/PizzaDetail.jsx
+++ src/components/PizzaDetail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { pizzaData } from '../data/pizzas';
 import { makeStyles } from '@material-ui/core/styles';
@@ -10,6 +10,7 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import NextWeekOutlinedIcon from '@material-ui/icons/NextWeekOutlined';
+import { ContextData } from './AppProvider';
 import '../css/PizzaDetail.css';
 
 
@@ -36,53 +37,61 @@ const useStyles = makeStyles({
 });
 
 const PizzaDetail = ({match}) => {
+    const [quantity, setQuantity] = useState(0);
     const { params: {id} } = match;
     const pizza = pizzaData.find( pizza => pizza.id === parseInt(id, 10));
     const classes = useStyles();
 
     return (
-        <div className={classes.container}>
-            <Card className={classes.root} variant="outlined">
-            <CardContent>
-                <img src={pizza.img} alt={pizza.title} />
-                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                {pizza.title}
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                    Ingredients:
-                </Typography>
-            </CardContent>
-            <CardActions>
-            <TextField
-              id="pizza-quantity"
-              label="Quantity"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant="outlined"
-              placeholder="How many pizzas"
-              size="small"
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              endIcon={<AddShoppingCartIcon/>}
-            >
-              Add to shopping cart
-            </Button>
-            <Link to="/checkout">
+      <ContextData>
+        {({ setCart, cart }) => (
+          <div className={classes.container}>
+              <Card className={classes.root} variant="outlined">
+              <CardContent>
+                  <img src={pizza.img} alt={pizza.title} />
+                  <Typography className={classes.title} color="textSecondary" gutterBottom>
+                  {pizza.title}
+                  </Typography>
+                  <Typography className={classes.pos} color="textSecondary">
+                      Ingredients:
+                  </Typography>
+              </CardContent>
+              <CardActions>
+              <TextField
+                id="pizza-quantity"
+                label="Quantity"
+                type="number"
+                value={quantity}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="outlined"
+                placeholder="How many pizzas"
+                size="small"
+                onChange={({target: {value}}) => setQuantity(parseInt(value, 10))}
+              />
               <Button
                 variant="contained"
-                color="default"
-                endIcon={<NextWeekOutlinedIcon/>}
+                color="primary"
+                endIcon={<AddShoppingCartIcon/>}
+                onClick={ () => setCart([...cart, {...pizza, quantity}])}
               >
-                Buy
+                Add to shopping cart
               </Button>
-            </Link>
-            </CardActions>
-            </Card>
-        </div>
+              <Link to="/checkout">
+                <Button
+                  variant="contained"
+                  color="default"
+                  endIcon={<NextWeekOutlinedIcon/>}
+                >
+                  Buy
+                </Button>
+              </Link>
+              </CardActions>
+              </Card>
+          </div>
+        )}
+      </ContextData>
     );
 }
 
